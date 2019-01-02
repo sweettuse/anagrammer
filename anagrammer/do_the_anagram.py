c926c76f1187fa1bbbfd139eb2a2aab43e298455
adam cushner <adam.cushner@xaxis.com>
2019-01-02 07:50:03 -0500

    speed up/bug fix for caps

--- anagrammer/do_the_anagram.py
+++ anagrammer/do_the_anagram.py
@@ -1,7 +1,5 @@
-import shelve
-from collections import defaultdict, Counter
+from collections import defaultdict
 from functools import reduce
-from hashlib import md5
 from itertools import permutations, combinations
 
 import click
@@ -15,7 +13,7 @@ CACHED_WORDS_SHELF = './.words_cache'
 @click.option('-n', '--num-letters', type=int, default=None, help='show only words of `n` length')
 @click.option('-m', '--min-num-letters', default=3, help='show words >= `n` length')
 @click.option('-f', '--full', count=True, help='use bigger dictionary (can be used up to twice)')
-@click.option('-t', '--together', is_flag=True, help='words sorted together regardless of length', default=False)
+@click.option('-t', '--together', is_flag=True, help='words sorted together regardless of length')
 @click.argument('word', nargs=1, required=True)
 def do_the_anagram(num_letters, word, full, together, min_num_letters):
     """calculate anagrams based on inputs"""
@@ -31,7 +29,6 @@ def do_the_anagram(num_letters, word, full, together, min_num_letters):
             print(*args)
 
     res = set()
-
     func = _do_the_anagram_big if len(word) > 9 else _do_the_anagram_small
     for n, curr in func(fn, word, r):
         p('=================')
@@ -53,10 +50,10 @@ def _do_the_anagram_big(fn, word, rng):
     use combinations to find anagrams
     faster with bigger words
     """
-    wbc = get_words_by_counter(fn)
+    wbc = _get_words_by_counter(fn)
     for n in rng:
-        counters = [wbc.get(_hash_dict(Counter(w)), set()) for w in combinations(word, n)]
-        yield n, reduce(set().union, counters)
+        words = (wbc.get(tuple(sorted(w)), set()) for w in combinations(word, n))
+        yield n, reduce(set().union, words)
 
 
 def _do_the_anagram_small(fn, word, rng):
@@ -71,36 +68,14 @@ def _do_the_anagram_small(fn, word, rng):
         yield n, {w for w in permutations(word, n) if w in words}
 
 
-def _hash_dict(d):
-    return hash(frozenset(sorted(d.items())))
-
-
 def _get_words_by_counter(fn):
-    """
-    helper func that actually reads in a file and transforms it
-    into a good data structure for use with combinations
-    """
-
+    """convert file into data structure for use by combinations"""
     res = defaultdict(set)
     with open(fn) as f:
-        for l in map(str.strip, f):
-            res[_hash_dict(Counter(l))].add(l)
+        for l in f:
+            l = l.strip().lower()
+            res[tuple(sorted(l))].add(l)
     return res
-
-
-def get_words_by_counter(fn):
-    """cache data structure used for combinations as creation can be expensive"""
-
-    with shelve.open(CACHED_WORDS_SHELF) as db:
-        with open(fn, 'rb') as f:
-            md5_sum = md5(f.read()).hexdigest()
-
-        key = str((fn, md5_sum))
-        res = db.get(key)
-        if not res:
-            res = _get_words_by_counter(fn)
-            db[key] = res
-        return res
 
 
 def __main():
@@ -109,4 +84,3 @@ def __main():
 
 if __name__ == '__main__':
     __main()
-
